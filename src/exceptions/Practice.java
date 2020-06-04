3f55f4b12374dd4b44c2a9a7c040deb81df9221b
Swaroop Uprety <breadfan18@gmail.com>
2020-06-04 14:01:39 -0400

    playing around

--- src/exceptions/Practice.java
+++ src/exceptions/Practice.java
@@ -6,7 +6,8 @@ public class Practice {
 
     public static void main(String[] args) {
         Testing testing = new Testing();
-        String totalString = testing.getString("Hello", "Team");
+
+        int totalString = testing.getString(4, 2);
 
         System.out.println(totalString);
 
--- src/exceptions/Testing.java
+++ src/exceptions/Testing.java
@@ -2,8 +2,8 @@ package exceptions;
 
 public class Testing {
 
-    public String getString (String a, String b) {
-        return a + " " + b;
+    public int getString (int a, int b) {
+        return a + b;
 
     }
 }
