f37ddfdacf10221d7438868b2843d9c962de6a82
Swaroop Uprety <breadfan18@gmail.com>
2020-06-04 14:05:42 -0400

    added a method getList in hte Testing class

--- src/exceptions/Testing.java
+++ src/exceptions/Testing.java
@@ -1,9 +1,21 @@
 package exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Testing {
 
     public int getString (int a, int b) {
         return a + b;
+
+    }
+
+    public List<Integer> getList () {
+        List<Integer> testList = new ArrayList<>();
+
+        testList.add(1);
+        testList.add(3);
+        testList.add(3);
 
     }
 }
