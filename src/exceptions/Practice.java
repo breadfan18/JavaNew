e172cfbe6d34b94771e062bbfb43dd760711b26b
Swaroop Uprety <breadfan18@gmail.com>
2020-06-04 13:45:38 -0400

    extended Practice to Testing

--- src/exceptions/Practice.java
+++ src/exceptions/Practice.java
@@ -1,6 +1,8 @@
 package exceptions;
 
-public class Practice {
+public class Practice extends Testing {
+
+
     public static void main(String[] args) {
         String totalString = getString("Hello", "Team");
 
