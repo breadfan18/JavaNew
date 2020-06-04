cb759107d3e84996d0446c562b995d38a34954e7
Swaroop Uprety <breadfan18@gmail.com>
2020-06-04 14:11:24 -0400

    added try catch to handle listindexoutofbounds

--- src/exceptions/Practice.java
+++ src/exceptions/Practice.java
@@ -11,6 +11,14 @@ public class Practice {
 
         System.out.println(totalInt);
 
+        try {
+            System.out.println(testing.getList().get(9));
+        } catch (Exception e) {
+            System.out.println("That list doesnt have that many values");
+        }
+
+        System.out.println("Code moves on");
+
     }
 
 
