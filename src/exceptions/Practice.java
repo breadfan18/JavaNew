2affce4689c52d6072dfdf86a0b77eb082766d12
Swaroop Uprety <breadfan18@gmail.com>
2020-06-16 16:20:42 -0400

    commit for the day

--- src/exceptions/Practice.java
+++ src/exceptions/Practice.java
@@ -17,6 +17,7 @@ public class Practice {
 
         System.out.println("Code moves on");
         System.out.println("Code moves on");
+        System.out.println("Cmd  d is pretty  cool");
 
     }
 
