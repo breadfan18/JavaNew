c2c3514c0dcae48cc208c004f7a221f3c2d40e3c
Swaroop Uprety <breadfan18@gmail.com>
2020-07-04 19:15:04 -0400

    Stringbuildere example

--- src/newPractice/MathPractice.java
+++ src/newPractice/MathPractice.java
@@ -1,5 +1,7 @@
 package newPractice;
 
+import java.text.NumberFormat;
+
 public class MathPractice {
 
     public static void main(String[] args) {
@@ -7,5 +9,17 @@ public class MathPractice {
         double doubleValue   = -3.99999;
         long rounded = Math.round(doubleValue);
         System.out.println("Rounded: " + rounded);
+
+        int intValue = 42;
+        String fromInt = Integer.toString(intValue);
+
+        System.out.println(fromInt);
+
+        long longValue = 10_000_000;
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        String formatted = formatter.format(longValue);
+        System.out.println(formatted);
+
+
     }
 }
