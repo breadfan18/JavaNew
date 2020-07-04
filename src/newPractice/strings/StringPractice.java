8e35d6d35dec27ccb03d98715d7a38d1e9a3baae
Swaroop Uprety <breadfan18@gmail.com>
2020-07-04 18:42:14 -0400

    first commit of the day

---
+++ src/newPractice/strings/StringPractice.java
@@ -0,0 +1,12 @@
+package newPractice.strings;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+        String s1 = "This is a string";
+        System.out.println(s1);
+
+
+
+    }
+}
