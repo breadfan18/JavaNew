b7f824b946f2527a89ee92571d8450ddc0844630
Swaroop Uprety <breadfan18@gmail.com>
2020-07-05 19:19:10 -0400

    completed calc app with exception handling

--- src/simpleCalculator/Calc.java
+++ src/simpleCalculator/Calc.java
@@ -6,46 +6,62 @@ public class Calc {
 
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter numeric value 1: ");
-        double number1 = scan.nextDouble();
+            String number1 =  getInput("Enter number 1: ");
+            String number2 =  getInput("Enter number 2: ");
+            String operation = getInput("Choose an operation  (+ -  * /): ");
 
-        System.out.print("Enter numeric value 2: ");
-        double number2 = scan.nextDouble();
+            double result = 0;
 
-        System.out.print("Choose an operation  (+ -  * /): ");
-        String operation = scan.next();
+        try {
+            switch (operation) {
+                case "+":
+                    result =  add(number1, number2);
+                    break;
+                case "-":
+                    result = subtract(number1, number2);
+                    break;
+                case "*":
+                    result = multiply(number1, number2);
+                    break;
+                case "/":
+                    result = divide(number1, number2);
+                    break;
+                default:
+                    System.out.println("Unrecognized operation");
+            }
 
-        switch (operation){
-            case "+":
-                System.out.println("The answer is: " + add(number1, number2));
-                break;
-            case "-":
-                System.out.println("The answer is: " + subtract(number1, number2));
-                break;
-            case "*":
-                System.out.println("The answer is: " + multiply(number1, number2));
-                break;
-            case "/":
-                System.out.println("The answer is: " + divide(number1, number2));
+            System.out.println("The answer is: " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("Number Formatting Exception " + e.getMessage());
         }
     }
 
-    private static double add (double a, double b) {
-        return a+b;
+    private static double add (String a, String b) {
+        double d1 = Double.parseDouble(a);
+        double d2 = Double.parseDouble(b);
+        return d1+d2;
     }
 
-    private static double subtract (double a, double b) {
-        return a-b;
-    }
+    private static double subtract (String a, String b) {
+        double d1 = Double.parseDouble(a);
+        double d2 = Double.parseDouble(b);
+        return d1-d2;    }
 
-    private static double multiply (double a, double b) {
-        return a*b;
-    }
+    private static double multiply (String a, String b) {
+        double d1 = Double.parseDouble(a);
+        double d2 = Double.parseDouble(b);
+        return d1*d2;    }
 
-    private static double divide (double a, double b) {
-        return a/b;
+    private static double divide (String a, String b) {
+        double d1 = Double.parseDouble(a);
+        double d2 = Double.parseDouble(b);
+        return d1/d2;    }
+
+    private static String getInput(String prompt){
+            Scanner scan = new Scanner(System.in);
+            System.out.print(prompt);
+            return scan.next();
     }
 
 
