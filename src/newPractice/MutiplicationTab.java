a34d7b5ef794971808180540b812b207dddd82f7
Swaroop Uprety <breadfan18@gmail.com>
2020-07-09 13:48:48 -0400

    some minor edits

---
+++ src/newPractice/MutiplicationTab.java
@@ -0,0 +1,22 @@
+package newPractice;
+
+import java.util.Scanner;
+
+public class MutiplicationTab {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int selectedNum;
+
+        System.out.println("Enter an integer: ");
+            selectedNum = sc.nextInt();
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(selectedNum + "x" + i + " = " + selectedNum * i);
+            }
+
+
+
+    }
+}
