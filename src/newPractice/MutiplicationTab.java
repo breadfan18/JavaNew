7b3cd6e4b62e1c3d9cbedbc4dc3ebcc5f9a267a8
Swaroop Uprety <breadfan18@gmail.com>
2020-07-10 13:39:05 -0400

    just trying to get a commit in

--- src/newPractice/MutiplicationTab.java
+++ src/newPractice/MutiplicationTab.java
@@ -14,6 +14,7 @@ public class MutiplicationTab {
 
             for (int i = 1; i <= 10; i++) {
                 System.out.println(selectedNum + "x" + i + " = " + selectedNum * i);
+
             }
 
 
